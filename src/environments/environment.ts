43aeee4c39e12165012cec3c024c92d27b2ba3de
Alejandro Gil <alejandrog@4waystech.com>
2025-06-23 13:15:32 -0400

    prod envirorment adjust

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    // apiUrl: 'https://app.findit-store.com/api/',
-    apiUrl: 'https://localhost:5051/api/'
+    apiUrl: 'https://app.findit-store.com/api/',
+    // apiUrl: 'https://localhost:5051/api/'
 };
